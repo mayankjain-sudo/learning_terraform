147bf7387468dd46941cbbf41b001a40e0d5a9a6
Mayank Jain <immanku7@gmail.com>
2024-07-18 16:46:30 +0800

    Learning Workspaces for maintaining the environment

---
+++ learning_workspaces/modules/ec2_instance/terraform.tfvars
@@ -0,0 +1,3 @@
+# File is used for passing the variables 
+ami_id="ami-0ad21ae1d0696ad58"
+instance_type="t2.micro"
---
+++ learning_workspaces/modules/ec2_instance/variables.tf
@@ -0,0 +1,6 @@
+variable "ami_id" {
+    description = "The AMI to use for the instance"
+}
+variable "instance_type" {
+    description = "The type of instance to run"
+}
