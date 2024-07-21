4f2bb3af3c601a35431cfdb36595bb3978fc9073
Mayank Jain <immanku7@gmail.com>
2024-07-21 15:42:06 +0800

    update learning

---
+++ aws/ha-instance/variables.tf
@@ -0,0 +1,18 @@
+variable "ami_id" {
+    type = string
+}
+variable "instance_type" {
+    type = string
+}
+variable "subnet1" {
+    type = string
+}
+variable "subnet2" {
+    type = string
+}
+variable "subnet3" {
+    type = string
+}
+variable "vpc_id" {
+    type = string
+}
