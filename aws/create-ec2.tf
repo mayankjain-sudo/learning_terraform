13e3304d57318cc9a55d8e9927a07137ee1ba525
Mayank Jain <immanku7@gmail.com>
2024-07-21 15:57:10 +0800

    remove old files

--- aws/create-ec2.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  profile    = "default"
-  region     = "ap-south-1"
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-0d2692b6acea72ee6"
-  instance_type = "t2.micro"
-}
